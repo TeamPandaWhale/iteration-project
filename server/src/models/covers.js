6aae0d9bbaaefa09d8201229da783201d75cab76
Olivia Carlisle <151247171+oliviacarlisle@users.noreply.github.com>
2024-05-02 01:37:47 -0400

    Add skeleton for covers.js

---
+++ server/src/models/covers.js
@@ -0,0 +1,16 @@
+export const idIndex = new Map();
+export const gameIdIndex = new Map();
+
+export const loadCovers = () => {};
+
+const Covers = {};
+
+Covers.findOneById = (id) => {};
+
+Covers.findOneByGameId = (gameId) => {};
+
+Covers.findAllByIds = (ids) => {};
+
+Covers.findAllByGameIds = (gameIds) => {};
+
+export default Covers;
